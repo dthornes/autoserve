9f49ed67f2751d0581d380f3916485e6c6ce6824
dthornes <65418584+dthornes@users.noreply.github.com>
2024-03-06 09:33:38 +0000

    Add logger for webhook

--- app/api/webhook/signable/route.ts
+++ app/api/webhook/signable/route.ts
@@ -1,10 +1,11 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { logger } from "@/logger";
 
 export async function GET(req: Request) {
 	// Get the body
-	console.log(req.body)
+	logger.info("Webhook test: ", req.body);
 
 	// Based on response update db record of customer
 
